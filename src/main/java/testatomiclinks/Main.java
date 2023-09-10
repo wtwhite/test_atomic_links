b73b95a8ff827bc53a03eab0c662e142308c3f4a
Tim White <walton.timothy.james.white@gmail.com>
2023-09-10 23:35:22 +1200

    Files.createLink() works on Linux, throws FileAlreadyExistsException when expected :)

--- src/main/java/testatomiclinks/Main.java
+++ src/main/java/testatomiclinks/Main.java
@@ -1,7 +1,11 @@
 package testatomiclinks;
 
+import java.nio.file.*;
+import java.io.File;
+
 public class Main {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		System.out.println("Hello, world!");
+		Files.createLink(new File("a_hardlink").toPath(), new File("a").toPath());
 	}
 }
